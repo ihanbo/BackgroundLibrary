a7a29ff3c0f214c16ccce8ee4940ca5c8c88a558
hanbo <hanbov5@gmail.com>
2018-11-18 11:22:54 +0800

    no message

--- library/src/main/java/com/noober/background/PPInject2.java
+++ library/src/main/java/com/noober/background/PPInject2.java
@@ -2,7 +2,6 @@ package com.noober.background;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -11,38 +10,35 @@ import android.view.ViewGroup;
 
 import org.xmlpull.v1.XmlPullParser;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CopyOnWriteArraySet;
-
 /**
  * @author hanbo
  * @date 2018/11/16
  */
-public class PPInject {
+public class PPInject2 {
     private LayoutInflaterWrapper mInflater;
 
-    public PPInject(BackgroundFactory backgroundFactory) {
+    public PPInject2(BackgroundFactory backgroundFactory) {
 
     }
 
     public Object getInflater(Activity activity) {
         if (mInflater == null) {
             mInflater = new LayoutInflaterWrapper(LayoutInflater.from(activity.getBaseContext()).cloneInContext(activity), activity);
-            setFactory3(new BackgroundFactory());
         }
         return mInflater;
     }
 
 
     public void setFactory3(Factory3 factory3) {
-        mInflater.setFactory3(factory3);
+        if (mInflater.mEF != null) {
+            mInflater.mEF.mFactory3 = factory3;
+        }
     }
 
     public void setExtraFactory2(LayoutInflater.Factory2 extraFactory2) {
-        mInflater.setFactory2(extraFactory2);
+        if (mInflater.mEF != null) {
+            mInflater.mEF.mExtraFactory2 = extraFactory2;
+        }
     }
 
 
@@ -55,31 +51,30 @@ public class PPInject {
         LayoutInflaterWrapper(LayoutInflater original, Context newContext) {
             super(original, newContext);
             mDelegate = original;
-            mDelegate.setFactory2(mEF = new ExpandableFactory());
         }
 
         @Override
-        public void setFactory(final Factory factory) {
-            mEF.f2s.add(new Factory2() {
-                @Override
-                public View onCreateView(View view, String s, Context context, AttributeSet attributeSet) {
-                    return onCreateView(s, context, attributeSet);
-                }
-
-                @Override
-                public View onCreateView(String s, Context context, AttributeSet attributeSet) {
-                    return factory.onCreateView(s, context, attributeSet);
-                }
-            });
+        public void setFactory(Factory factory) {
+            if (mFactorySet) {
+                throw new IllegalStateException("A factory has already been set on this LayoutInflater");
+            }
+            if (factory == null) {
+                throw new NullPointerException("Given factory can not be null");
+            }
+            mFactorySet = true;
+            super.setFactory(mEF = new ExpandableFactory(factory));
         }
 
         @Override
         public void setFactory2(Factory2 factory) {
-            mEF.f2s.add(factory);
-        }
-
-        public void setFactory3(Factory3 factory) {
-            mEF.f3s.add(factory);
+            if (mFactorySet) {
+                throw new IllegalStateException("A factory has already been set on this LayoutInflater");
+            }
+            if (factory == null) {
+                throw new NullPointerException("Given factory can not be null");
+            }
+            mFactorySet = true;
+            super.setFactory2(mEF = new ExpandableFactory(factory));
         }
 
         @Override
@@ -128,26 +123,41 @@ public class PPInject {
 
 
     private static class ExpandableFactory implements LayoutInflater.Factory2 {
-        CopyOnWriteArrayList<LayoutInflater.Factory2> f2s = new CopyOnWriteArrayList<>();
-        CopyOnWriteArrayList<Factory3> f3s = new CopyOnWriteArrayList<>();
+        LayoutInflater.Factory2 mExtraFactory2;
+        Factory3 mFactory3;
+
+        LayoutInflater.Factory2 mDelegate2; //代理的
+        LayoutInflater.Factory mDelegate;   //代理的
+        boolean isFactoty2;
+
+        ExpandableFactory(LayoutInflater.Factory2 delegate) {
+            mDelegate2 = delegate;
+            isFactoty2 = true;
+        }
+
+        ExpandableFactory(LayoutInflater.Factory factory) {
+            mDelegate = factory;
+            isFactoty2 = false;
+        }
 
         @Override
         public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
             View view = null;
 
-            if (!f2s.isEmpty()) {
-                for (int i = 0; i < f2s.size(); i++) {
-                    view = f2s.get(i).onCreateView(parent, name, context, attrs);
-                    if (view != null) {
-                        break;
-                    }
+            if (mExtraFactory2 != null) {
+                view = mExtraFactory2.onCreateView(parent, name, context, attrs);
+            }
+
+            if (view == null) {
+                if (isFactoty2) {
+                    view = mDelegate2.onCreateView(parent, name, context, attrs);
+                } else {
+                    view = mDelegate.onCreateView(name, context, attrs);
                 }
             }
 
-            if (view != null && !f3s.isEmpty()) {
-                for (int i = 0; i < f3s.size(); i++) {
-                    f3s.get(i).onDecorateView(view, name, context, attrs, parent);
-                }
+            if (view != null && mFactory3 != null) {
+                mFactory3.onDecorateView(view, name, context, attrs, parent);
             }
 
             return view;
